4f83952fe253f0d121e749e9ab76d79bafeb666c
dandrews <daniel.raymond.andrews@gmail.com>
2013-04-18 06:41:15 +0800

    mixpanel tracking of startup interactions

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.destroy_all
+UserPing.destroy_all
 Startup.destroy_all
 
 Startup.create([
